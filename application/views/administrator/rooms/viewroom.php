323eb6332b4151c57483ff42f008f9c76eb4a8d8
krrevilla <karlangelorevilla@gmail.com>
2016-06-30 00:32:43 +0800

    changed viewroom.php

--- application/views/administrator/rooms/viewroom.php
+++ application/views/administrator/rooms/viewroom.php
@@ -31,13 +31,13 @@
                     echo "<td>Bed Available</td>";
                     echo "<td>Empty</td>";
                     echo "<td>";
-                      echo "<a href='".base_url()."Admin/remove_bed/".$roomid."/".$room['bed_id']."' role='button' class='btn btn-default btn-xs'>Remove Bed</a>";
+                      echo "<a href='".base_url()."Admin/remove_bed/".$roomid."/".$room['bed_id']."' role='button' class='btn btn-default btn-xs' >Remove Bed</a>";
                     echo "</td>";
                 }else{
                   echo "<td>OCCUPIED</td>";
                   echo "<td>".$room['first_name']." ".$room['last_name']."</td>";
                   echo "<td>";
-                    echo "<a href='".base_url()."Admin/remove_bed/".$roomid."/".$room['bed_id']."' role='button' class='btn btn-default btn-xs' disabled>Remove Bed</a>";
+                    echo "<a href='javascript: void(0)' role='button' class='btn btn-default btn-xs'>Remove Bed</a>";
                   echo "</td>";
                 }
 
