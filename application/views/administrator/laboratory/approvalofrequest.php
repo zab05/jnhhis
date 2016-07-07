9b530d6f445ec0fb7038282623d8f94c58a24047
Cyrus Lacson <lacsoncyrus2@gmail.com>
2016-07-07 21:14:24 +0800

    Revisions and Additions
    Revs and Adds

--- application/views/administrator/laboratory/approvalofrequest.php
+++ application/views/administrator/laboratory/approvalofrequest.php
@@ -1,11 +1,25 @@
 <section id="main-content">
   <section class="wrapper">
     <div class="row">
-      <div class="col-sm-12">
+      <div class="col-sm-3">
+          <section class="panel">
+              <header class="panel-heading" style="background-color: #000;"></header>
+              <table class="table">
+                <tr>
+                  <td colspan="2" align="center"><h5><a class="btn btn-info" href="<?=base_url()?>Admin/LabAccRequest">View Accepted Requests</a></h5></td>
+                </tr>
+                <table class="table">
+                  <tr>
+                    <td colspan="2" align="center"><h5><a class="btn btn-info" style="background-color:red;" href="<?=base_url()?>Admin/LabRejRequest">View Rejected Requests</a></h5></td>
+                  </tr>
+              </table>
+          </section>
+      </div>
+      <div class="col-sm-9">
           <section class="panel">
               <header class="panel-heading" style="background-color: #000;"></header>
               <header class="panel-heading">
-                  <center><h4>LABORATORY REQUEST<h4></center>
+                  <center><h4>PENDING LABORATORY REQUEST<h4></center>
               </header>
               <table class="table table-hovered" style="text-align: center;">
                 <tr id="tblheader">
@@ -18,25 +32,21 @@
                 </tr>
               <?php
               foreach($laboratoryreq as $labreq){
+              if($labreq['lab_status']==1){
                 echo "<tr>";
                 echo"<td>".$labreq['lab_id']."</td>";
                 echo"<td>".$labreq['lab_date_req']."</td>";
                 echo"<td>".$labreq['lab_patient_checkin']."</td>";
                 echo"<td>".$labreq['first_name']." ".$labreq['last_name']."</td>";
-                if($labreq['lab_status']==1){
                 echo "<td>For Approval</td>";
-              } elseif ($labreq['lab_status']==2) {
-                echo "<td>Approved</td>";
-              } else {
-                echo "<td>Cancelled</td>";
-              }
                 echo "<td>";
                   echo "<a href='".base_url()."Admin/ShowLabReq/".$labreq['lab_id']."' role='button' class='btn btn-default btn-xs'>Show</a>";
                   echo " <a href='".base_url()."Admin/ApproveLabReq/".$labreq['lab_id']."' role='button' class='btn btn-default btn-xs'>Approve</a>";
-                  echo " <a href='".base_url()."Admin/CancelLabReq/".$labreq['lab_id']."' role='button' class='btn btn-default btn-xs'>Cancel</a>";
+                  echo " <a href='".base_url()."Admin/CancelLabReq/".$labreq['lab_id']."' role='button' class='btn btn-default btn-xs'>Reject</a>";
                 echo "</td>";
                 echo "</tr>";
               }
+            }
 
               ?>
               </table>
