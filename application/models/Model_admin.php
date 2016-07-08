fede8ec304a1192cc365ffa00d170fe5b934ac11
Cyrus Lacson <lacsoncyrus2@gmail.com>
2016-07-08 12:00:02 +0800

    Additions and Revisions
    Purchasing ( CSR PART )

--- application/models/Model_admin.php
+++ application/models/Model_admin.php
@@ -819,5 +819,72 @@
       $query = $this->db->get();
       return $query->result_array();
     }
+
+    function get_request_data($id)
+    {
+      $this->db->select('*');
+      $this->db->from('purchasing_csr');
+      $this->db->where('purchase_id',$id);
+      $query = $this->db->get();
+      return $query->row();
+    }
+
+    function get_request_type($id)
+    {
+      $this->db->select('*');
+      $this->db->from('purchasing_csr');
+      $this->db->where('purchase_id',$id);
+      $query = $this->db->get();
+      return $query->row('request_type');
+    }
+
+    function change_pur_status($id,$newstat)
+    {
+      $this->db->where('purchase_id',$id);
+      $this->db->update('purchasing_csr',$newstat);
+    }
+
+    function insertnewcsrproduct($data)
+    {
+      $this->db->insert('csr_inventory',$data);
+    }
+
+    function get_csr_stock($id)
+    {
+      $this->db->select('*');
+      $this->db->from('csr_inventory');
+      $this->db->where('csr_id',$id);
+      $query = $this->db->get();
+      return $query->row('item_stock');
+    }
+
+    function restockcsrproduct($id,$data)
+    {
+      $this->db->where('csr_id',$id);
+      $this->db->update('csr_inventory',$data);
+    }
+
+    function get_acceptedcsr_requests()
+    {
+      $this->db->select('*');
+      $this->db->from('purchasing_csr a');
+      $this->db->join('users u','a.requester_id=u.user_id','left');
+      $this->db->join('purchase_req_type p', 'a.request_type=p.pur_req_id', 'left');
+      $this->db->where('pur_stat',1);
+      $query = $this->db->get();
+      return $query->result_array();
+    }
+    function get_rejectedcsr_requests()
+    {
+      $this->db->select('*');
+      $this->db->from('purchasing_csr a');
+      $this->db->join('users u','a.requester_id=u.user_id','left');
+      $this->db->join('purchase_req_type p', 'a.request_type=p.pur_req_id', 'left');
+      $this->db->where('pur_stat',2);
+      $query = $this->db->get();
+      return $query->result_array();
+    }
+
+
   }
 ?>
