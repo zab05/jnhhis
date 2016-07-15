2b32ee26ec2c499be91a1677a2d15a9aa43da723
magmagg <lorenzomagno2004@gmail.com>
2016-07-15 13:11:34 +0800

    Pharmacy request

---
+++ application/views/administrator/pharmacy/accept_pharmacy_request_modal.php
@@ -0,0 +1,16 @@
+<div class="modal fade" id="confirm-accept" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                Accept request
+            </div>
+            <div class="modal-body">
+              Accept this request?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                <a style="margin-top: -5px;" class="btn btn-success btn-ok">Accept</a>
+            </div>
+        </div>
+    </div>
+</div>
